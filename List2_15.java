d7f0b43cd1bd74503aa54eb497326616eb780b5f
hakutosuzuki <hakutosuzuki@crooz.co.jp>
2015-12-15 10:38:46 +0900

    12/04 Decision Positive and Negative

--- List2_15.java
+++ List2_15.java
@@ -11,7 +11,7 @@ public class List2_15 {
 	   	//0~3までの乱数を生成し、fortuneに格納
 	   	int fortune = new java.util.Random().nextInt(4);
         System.out.println("a="+fortune);
-        //  入力した値が、正の数かどうかを調べる
+        //入力した値が、正の数かどうかを調べる
         if(fortune > 0){
             System.out.println("aは正の数です。");  //  正の数だった場合に実行
         	} else {
