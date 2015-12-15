36d3a9f04d648137c4f1efa51f73694e7a804e60
hakutosuzuki <hakutosuzuki@crooz.co.jp>
2015-12-15 10:37:28 +0900

    12/08 ArrayPractice

--- hakuto_java03.java
+++ hakuto_java03.java
@@ -9,13 +9,13 @@ import java.util.Scanner;
 public class List2_19 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-        //インデックス入力
+        	//インデックス入力
 		int num;
 		System.out.print("インデックスを入力＞");
 		num = scan.nextInt();
-        //10個の配列を生成
+        	//10個の配列を生成
 		int[] a = new int[10];
-        //i+1を配列の中身に代入
+        	//i+1を配列の中身に代入
 		for (int i = 0; i < a.length; i++) {
 			a[i] = i + 1 ;
 		}
